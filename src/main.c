b919b5bae0ad4c77e534c23011ef895402191ff2
Elena <razuline@icloud.com>
2025-06-12 19:02:24 +0200

    minishell history/readline

--- src/main.c
+++ src/main.c
@@ -6,19 +6,22 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 17:44:19 by erazumov          #+#    #+#             */
-/*   Updated: 2025/06/11 17:53:49 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/06/12 18:19:53 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include <stdlib.h>
+# include <readline/history.h>
+#include <readline/readline.h>
+//#include "../include/minishell.h"
 
 int	main(void)
 {
 	char	*line;
 	// Configure le gestionnaire de signal pour Ctrl+C
-	signal(SIGINT, handle_sigint);
+	// signal(SIGINT, handle_sigint);
 	// Ignorer Ctrl+\ (SIGQUIT) pendant la saisie du prompt
-	signal(SIGQUIT, SIG_IGN);
+	// signal(SIGQUIT, SIG_IGN);
 	while (1)
 	{
 		// 1. Afficher le prompt et lire la ligne
@@ -30,19 +33,21 @@ int	main(void)
 			break ;
 		}
 		// 3. Gérer les lignes vides ou avec seulement des espaces
-		if (*line == '\0' || is_whitespace(line))
+		if (*line == '\0') //|| is_whitespace(line))
 		// is_whitespace est une fonction à implémenter pour vérifier si la ligne est vide ou contient seulement des espaces
 		{
 			free(line);
 			continue ;
 		}
 		// 4. Ajouter la ligne à l'historique (pour les flèches haut/bas)
-		// add_history(line); // Décommenter quand la base est stable
+		if (line && *line)
+			add_history(line);
 		// --- POINT D'INTÉGRATION AVEC TON PARSER ---
 		// Ici viendront les appels à tes fonctions de lexing/parsing
 		printf("Tu as tapé : '%s'\n", line); // Pour le moment, juste un echo
 		free(line);
 	}
+	rl_clear_history();
 	return (0);
 }
 
@@ -56,7 +61,7 @@ int	is_whitespace(char *str)
 	}
 	return (1);
 }
-
+/*
 void	handle_sigint(int sig)
 {
 	(void)sig;
@@ -65,3 +70,4 @@ void	handle_sigint(int sig)
 	rl_replace_line("", 0);
 	rl_redisplay();
 }
+*/
