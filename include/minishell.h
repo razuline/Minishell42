3fa257e8eed87d4df9f16bdf23f5807058f13283
Elena <razuline@icloud.com>
2025-06-17 13:58:59 +0200

    libft in process

--- include/minishell.h
+++ include/minishell.h
@@ -6,22 +6,37 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 17:45:03 by erazumov          #+#    #+#             */
-/*   Updated: 2025/06/11 17:54:26 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/06/17 13:38:44 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "../libft/include/libft.h"
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include "../libft/includes/libft.h"
 
-void	handle_sigint(int sig);
-int		is_whitespace(char *str);
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_REDIR_APPEND,
+	TOKEN_ERROR
+}			t_ttype;
 
+typedef struct s_token
+{
+	char	*value;
+	t_ttype	*type;
+}			t_token;
+
+//void	handle_sigint(int sig);
+int			is_whitespace(char *str);
 
 #endif
---
+++ libft/src/ft_lib/ft_lstnew.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 13:45:52 by erazumov          #+#    #+#             */
+/*   Updated: 2025/06/17 13:58:10 by erazumov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Allocates memory (using malloc(3)) and returns a new node. The ’content’
+member variable is initialized with the given parameter ’content’.
+The variable ’next’ is initialized to NULL. */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*lst;
+
+	lst = (t_list *)malloc(sizeof(*lst));
+	if (!lst)
+		return (NULL);
+	if (!content)
+	{
+		lst->content = NULL;
+		lst->content_size = 0;
+	}
+	else
+	{
+		lst->content = malloc(content_size);
+		if (!lst->content)
+			return (NULL);
+		ft_memcpy(lst->content, content, content_size);
+		lst->content_size = content_size;
+	}
+	lst->next = NULL;
+	return (lst);
+}
