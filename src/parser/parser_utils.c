5819b05abcf285484a0a28d93967f9b65892895a
Elena <razuline@icloud.com>
2025-08-09 17:25:59 +0200

    minihell parser UPD 9.08 razuline

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: preltien <preltien@student.42.fr>          +#+  +:+       +#+        */
+/*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 13:04:53 by erazumov          #+#    #+#             */
-/*   Updated: 2025/08/07 10:33:43 by preltien         ###   ########.fr       */
+/*   Updated: 2025/08/09 17:24:45 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ char	**create_argv(char **old_argv, char *new_str)
 	{
 		printf("[ERROR] create_argv: malloc failed\n");
 		return (NULL);
+	}
 	copy_old_argv(new_argv, old_argv, count);
 	new_argv[count] = strdup(new_str);
 	if (!new_argv[count])
